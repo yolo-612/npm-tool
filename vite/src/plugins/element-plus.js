eabd9041562b41d32843923f5aa7fd1a0ce502f1
戴金森 <daijinsen1@sfmail.sf-express.com>
2024-04-18 20:05:32 +0800

    feat(vite): 配置ui框架按需导入

--- vite/src/plugins/element-plus.js
+++ vite/src/plugins/element-plus.js
@@ -1,6 +1,5 @@
-import { ElButton } from "element-plus";
+// import { ElButton } from "element-plus";
 
-export default function ({router,app,pinia}) {
-  // 命名空间、多语言（修改的话才加上）
-  app.use(ElButton);
-}
+// export default function ({router,app,pinia}) {
+//   app.use(ElButton);
+// }
