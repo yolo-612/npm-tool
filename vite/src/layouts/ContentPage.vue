905e096acba9f2f949a6d1c195f2a5075b00619f
戴金森 <daijinsen1@sfmail.sf-express.com>
2024-04-21 14:49:44 +0800

    feat(vite): 增加loading的标签样式

--- vite/src/layouts/ContentPage.vue
+++ vite/src/layouts/ContentPage.vue
@@ -26,7 +26,7 @@ const props = defineProps({
 const route = useRoute();
 const routerTitle = computed(() => props.title || route.meta.title || '');
 const loading = ref(true);
-const loadingMessage = ref('');
+const loadingMessage = ref('加载中');
 const errorTitle = ref('加载错误');
 const errorMessage = ref('');
 
@@ -60,8 +60,9 @@ const onClickRetry = () => {
     <header class='header' v-if='showTitle'>
       <slot name="header"><span>{{ routerTitle }}</span></slot>
     </header>
-    <div class='content' v-loading='loading' :element-loading-text="loadingMessage">
-      <el-result v-if='!loading && errorMessage' icon="warning" :title='errorTitle'  :sub-title="errorMessage">
+    <div class='content'>
+      <div v-if='loading' class='loading' v-loading='true' :element-loading-text="loadingMessage"></div>
+      <el-result v-else-if='!loading && errorMessage' icon="warning" :title='errorTitle'  :sub-title="errorMessage">
         <template #extra>
           <el-button type="primary" @click='onClickRetry'>重新操作</el-button>
         </template>
@@ -73,6 +74,9 @@ const onClickRetry = () => {
 
 <style scoped>
 .ContentPage{
+  position: relative;
+  flex-direction: column;
+  display: flex;
   background-color: #FFFFFF;
   padding: 10px 20px;
   flex-shrink: 0;
@@ -89,7 +93,11 @@ const onClickRetry = () => {
   line-height: 22px;
   text-align: left;
 }
+.ContentPage .loading{
+  height: 200px;
+}
 .ContentPage .content{
-
+  flex-shrink: 0;
+  flex-grow: 1;
 }
 </style>
