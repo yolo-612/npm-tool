7c30a8b27faed0621718c95587dff2bba75a7248
keruisiya <2458097476@qq.com>
2025-03-03 21:17:58 +0800

    feat(vite): permission格式化

--- vite/src/directive/permission.ts
+++ vite/src/directive/permission.ts
@@ -1,3 +1,4 @@
+import type { DirectiveBinding } from 'vue';
 import usePermission from '@/hook/usePermission';
 
 /**
@@ -6,12 +7,12 @@ import usePermission from '@/hook/usePermission';
  */
 export default {
   // 在元素被插入到 DOM 前调用
-  mounted(el, binding) {
+  mounted(el: HTMLElement, binding: DirectiveBinding) {
     // value 获取用户使用自定义指令绑定的内容
-    const { value } = binding
+    const { value } = binding;
 
-    if(!usePermission().checkPermission(value) && el.parentNode){
+    if (!usePermission().checkPermission(value) && el.parentNode) {
       el.parentNode.removeChild(el);
     }
   },
-}
+};
