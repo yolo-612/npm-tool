39a29bde34a0f6e2edc6af77337b5501793341f5
keruisiya <2458097476@qq.com>
2025-04-13 12:44:14 +0800

    feat(admin): tab调整前端路由时候路由命名问题修复

--- vite/src/router/modules/dashboard.ts
+++ vite/src/router/modules/dashboard.ts
@@ -3,7 +3,7 @@ import { DEFAULT_LAYOUT } from '@/router/base';
 
 const dashboard: RouteRecordRaw = {
   path: '/dashboard/workplace',
-  name: 'workplace',
+  name: 'dashboard',
   component: DEFAULT_LAYOUT,
   meta: {
     title: '工作台-Router', // 页面tab显示的名称
@@ -14,7 +14,7 @@ const dashboard: RouteRecordRaw = {
   children: [
     {
       path: '',
-      name: 'workplacePage',
+      name: 'workplace',
       component: () => import('@/pages/dashboard/workplace.vue'),
       meta: {
         title: '工作台', // 页面tab显示的名称
